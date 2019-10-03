828388a12989887ae2c87f7b472e688efc2d946d
Husuch <nicolas.hausr@gmail.com>
2019-10-03 15:14:10 +0200

    Database

--- PhP/jukeBoxPlayer.php
+++ PhP/jukeBoxPlayer.php
@@ -30,51 +30,70 @@
 
     <div class="audio">
 
-        <?php
-
-        include_once("../Database/connection.php");
-        $con = new mysqli("localhost", "root", "", "jukebox");
-        $select = "Select * from music";
-        $result = mysqli_query($con, $select);
-        $style = $_POST['style'];
-        $loadSource = 0;
-        $source = array();
-        $i = 0;
-        while ($rows = mysqli_fetch_assoc($result)) {
-            if ($rows['Genre'] == $style) {
-
-                $source[$loadSource] = "../Mp3/" . $rows['Mp3Path'];
-                $loadSource = $loadSource + 1;
-            } else {
-                echo "";
-            }
-        }
-        ?>
-        <audio id="myTune" class="audio" controls>
-            <source src="<?php echo $source[0] ?>">
-
-            <audio id="music" src="blah.mp3" onended="myFunction()"></audio>
-            myFunction(){
-            }
+        <table>
+            <tr>
+                <th>Artist</th>
+                <th>Album</th>
+                <th>Song</th>
+                <th>Genre</th>
+            </tr>
 
             <?php
 
-            while ($loadSource - 1 > $i) {
+            include_once("../Database/connection.php");
+            $con = new mysqli("localhost", "root", "", "jukebox");
+            $select = "Select * from music";
+            $result = mysqli_query($con, $select);
+            $style = $_POST['style'];
+            $loadSource = 0;
+            $source = array();
+            $i = 0;
+            while ($rows = mysqli_fetch_assoc($result)) {
+                if ($rows['Genre'] == $style ) {
 
-                if (document.getElementById("myTune").currentTime == 90) {
-
-                    $i = $i + 1;
+                    $source[$loadSource] = "../Mp3/" . $rows['Mp3Path'];
+                    $loadSource = $loadSource + 1;
                     ?>
-                    <source src="<?php echo $source[$i] ?>">
+                    <tr>
+                        <td><?php echo $rows['Artist'] ?></td>
+                        <td><?php echo $rows['Album'] ?></td>
+                        <td><?php echo $rows['Song'] ?></td>
+                        <td><?php echo $rows['Genre'] ?></td>
+                    </tr>
                     <?php
-                }
-                else{
+                } else {
                     echo "";
                 }
             }
             ?>
-
+        </table>
+    </div>
+    <div class="sound">
+        <audio id="myTune" class="audio" controls>
+            <source src="<?php echo $source[0] ?>">
         </audio>
+        <script type="text/javascript">
+            document.getElementById('myTune').addEventListener("ended", function () {
+                <?php
+                $i = $i + 1;
+                nextSong($source[$i]);
+                ?>
+            });
+
+        </script>
+        <?php
+
+        function nextSong($source)
+        {
+            ?>
+            <audio id="myTune" class="audio" controls>
+                <source src="<?php echo $source ?>">
+            </audio>
+            <?php
+        }
+
+        ?>
+
         <button onclick="document.getElementById('myTune').play()">Play</button>
         <button onclick="document.getElementById('myTune').pause()">Pause</button>
         <button onclick="document.getElementById('myTune').pause(); document.getElementById('myTune').currentTime = 0;">
@@ -83,7 +102,10 @@
         <button onclick="document.getElementById('myTune').volume+=0.1">Volume Up</button>
         <button onclick="document.getElementById('myTune').currentTime+=30">Speed Up</button>
         <button onclick="document.getElementById('myTune').volume-=0.1">Volume Down</button>
-
+        <button onclick="document.getElementById('myTune').volume-=2">Mute</button>
+        <button onclick="document.getElementById('myTune').volume+=2">Unmute</button>
     </div>
+
+</div>
 </body>
 </html>
